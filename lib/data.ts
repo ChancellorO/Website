6886495f30e36eefacd560ac14b1fe1b7f236723
Chance Richey <crichey57@g.ucla.edu>
2024-04-04 16:30:26 -0700

    Updated information

--- lib/data.ts
+++ lib/data.ts
@@ -11,25 +11,25 @@ import SustainableEnergy from "@/public/sustainableenergy.png";
 export const projects = [
     {
         title: "Sustainable Energy",
-        description: "Constructed a series of ML models to identify leading carbon emission with above an 85% accuracy rate. Developed a time forecasting ARIMA model to predict the amount of carbon a country emits in future years.",
+        description: "Constructed a series of ML models to identify leading carbon emission with above a 90% accuracy rate. Developed a time forecasting ARIMA model, predicting the amount of carbon a country emits.",
         date: "October 2023 - December 2023",
         image: SustainableEnergy,
     },
     {
         title: "HTTP Web Server",
-        description: "Generated bash scripts to automate integration and unit tests to preserve the integrity of the web server. Adhered to Google's protocols for testing and refactoring, and established a standard for request handlers. Standardized NGINX config file formats and integrated corresponding test cases.",
+        description: "Followed test-driven development, Creating bash scripts for automated testing and developing unit tests using gtest/gmock. Standardized NGINX config file formats and developed corresponding request handlers. Integrated global coding style and constructed documentation to enable readability for potential contributors. Managed a team of 4 students, implementing agile methodologies by setting up sprint meetings and conducting code reviews.",
         date: "March 2023 - June 2023",
         image: Webserver,
     },
     {
         title: "WeTour",
-        description: "Created RESTful APIs using Django with CRUD operations to store and retrieve local tourist data. Engineered the authorization system to authenticate and preserve user data on PostgresSQL. Developed multiple responsive Django template pages using Tailwind CSS to enhance user experience. Guided a group of 4 students and presented the project to 20+ senior software engineers and 300+ people.",
+        description: "Created a RESTful API using Django with CRUD operations to store and retrieve local tourist data. Engineered the authorization system to authenticate and preserve user data on PostgresSQL. Developed multiple responsive Django template pages using Tailwind CSS to enhance user experience. Worked with a team of 4 students and presented the project to 20+ senior software engineers and 300+ people.",
         date: "April 2023",
         image: WeTour
     },
     {
         title: "OurAnimeList",
-        description: "Lead the development of the backend infrastructure using NodeJS and MongoDB database. Devised an algorithm leveraging users' anime preferences to match users with similar interests",
+        description: "Lead the development of the backend infrastructure using NodeJS and MongoDB database. Devised an algorithm leveraging users' anime preferences to match users with similar interests.",
         date: "March 2022 - June 2022",
         image: AnimeListPhoto,
     },
@@ -40,7 +40,7 @@ export const experiences = [
         title: "Software Engineer Intern",
         company: "University of California, Los Angeles",
         date: "April 2023 - Present",
-        description: "Constructing a mobile application using react-native to enhance digital outreach for mental health resources. Creating responsive pages and developing a RESTful API that manages data from MongoDB.",
+        description: "Developing proprietary tools for cultural departments and conducting codebase refactoring for programs used by 100+ users. Lead developer of a chatbot project, constructing a responsive frontend design and setting up backend logic with OpenAI API. Refactored SQL queries generating data tables, improving query time for the Visitors Bureau SQL Server.",        
         icon: UCLALogo,
 
     },
@@ -48,14 +48,14 @@ export const experiences = [
         title: "Mobile Developer",
         company: "IDONTMIND - Contract",
         date: "April 2023 - Present",
-        description: "Developing proprietary tools for multiple cultural departments and co-lead developer of a chatbot project. Leading the redesign of the SQL database and conducting codebase refactoring for programs used by 100+ users.",        
+        description: "Constructing a mobile application to enhance digital outreach for mental health resources on IOS and Android devices. Creating responsive frontend pages such as Content Library and Day Challenge using React Native and CSS. Implemented logic for “Recommendation” and “Favorite” features and designed the schema for users in NoSQL database. Developed Backend endpoints and linked them to frontend pages.",        
         icon: IDONTMINDLogo,
     },    
     {
         title: "Computer Science Mentor and Tutor",
         company: "College of the Canyons",
         date: "October 2022 - December 2023",
-        description: "Taught 25+ students complex computer science topics regarding Data Structures, Algorithms, and Discrete Structures. Assisted 1st and 2nd year students in finding opportunities outside of academia and prepared them to transfer to four-year universities.",        
+        description: "Taught 25+ students computer science topics regarding Data Structures, Algorithms, and Discrete Structures. Held computer science study sessions for Java and C++ courses, helping students understand complex subjects such as OOP. Assisted students for transferring to four-year universities, Scheduling weekly meetings and curating academic plans.",
         icon: COCLogo,        
     },
 ] as const;
