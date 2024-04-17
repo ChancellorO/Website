fbbe64b07aeb4c8fabb122838f93648513fba41d
ChancellorO <chancellororichey@gmail.com>
2024-04-16 17:09:11 -0700

    updated mobile view

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,17 +8,21 @@ import { Icon } from "@iconify/react";
 export default function Navbar() {
     const [activeNavbar, setActiveNavbar] = useState(false);
     const toggleNavbar = () => {
+        console.log("pressed");
         setActiveNavbar(!activeNavbar);
     }
     return(
         <header className="z-[999] relative">
-            <nav className="flex fixed justify-between p-4 px-64 w-screen shadow-lg bg-zinc-900 bg-opacity-75 backdrop-blur-[0.5rem]">
-                <h1 className="text-3xl">
-                    <Link href="#top">
-                        {"<"}Chancellor Richey{"/>"}
-                    </Link>                     
+            <nav className="flex flex-col md:flex-row fixed justify-between p-4 lg:px-48 w-screen shadow-lg bg-zinc-900 bg-opacity-75 backdrop-blur-[0.5rem]">
+                <div className="flex justify-between">
+                    <h1 className="text-3xl">
+                        <Link href="#top">
+                            {"<"}Chancellor Richey{"/>"}
+                        </Link>                     
                     </h1>
-                <ul className="text-xl flex items-center gap-7 text-[0.9rem] justify-center">
+                    <Icon icon='mdi:hamburger-menu' className="text-5xl md:hidden" onClick={toggleNavbar}/>
+                </div>                                
+                <ul className={`text-xl flex ${ activeNavbar ? "flex-col" : "hidden" } md:flex md:flex-row items-center gap-7 text-[0.9rem] justify-center `}>
                     <li>
                         <Link href="#about">
                             About
